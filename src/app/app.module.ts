ec301b333bbf970352686b9910b91ff529368f51
Ellian Carlos Oliveira Costa <elliancosta@tokenlab.com.br>
2020-06-30 17:11:35 -0300

    Barra superior adaptada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+// import {  } from '@angular/material/';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutmeComponent, NavigationbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatMenuModule,
     MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
     MatButtonModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/navigationbar/navigationbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigationbar',
+  templateUrl: './navigationbar.component.html',
+  styleUrls: ['./navigationbar.component.scss']
+})
+export class NavigationbarComponent {
+
+  constructor() {}
+
+}
